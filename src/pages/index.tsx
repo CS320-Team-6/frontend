62201ddf392406bc5cdfa7c8f854e97060d60a09
gwarren3210 <gavrielewarren@gmail.com>
2023-04-06 13:25:35 -0400

    first working post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,23 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import { useState } from 'react';
-import { TextField, MenuItem, Box } from '@mui/material';
-//import { DatePicker } from '@mui/x-date-pickers';
-//import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
+import { TextField, MenuItem, Box, Button } from '@mui/material';
 
 export default function Home() {
   const [Id, setId] = useState('');
   const [date, setDate] = useState(new Date());
-  const [priority, setPriority] = useState('Loading...'); // LOW, MEDIUM, HIGH, URGENT
-  //const [status, setStatus] = useState('');
-  //const [mail, setMail] = useState('Loading...');
-  const [data, setData] = useState('Loading...');
-  const [sev, setSev] = useState('Small');
-  const URL = 'http://urepairfreejohn1-env.eba-we2tn3yk.us-east-2.elasticbeanstalk.com/equipment/1'
-    
-  // const [hasData, setHasData] = useState(false)
-  const severities = [
+  const [priority, setPriority] = useState('LOW'); // LOW, MEDIUM, HIGH, URGENT
+  const [data, setData] = useState(''); // not used
+  const URL = 'http://urepair-env.eba-hnfscrcj.us-east-2.elasticbeanstalk.com/issue'
+  const dateJSON = {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1, // add 1 because getMonth() returns 0-based index
+    day: date.getDate(),
+    hour: date.getHours(),
+    minute: date.getMinutes()
+  };
+
+  const priorities = [
     {
       value: 'LOW',
       label: 'Low',
@@ -36,6 +36,7 @@ export default function Home() {
       label: 'Urgent',
     },
   ];
+
   const getData = async () => {
     console.log('getData\n');
     const res = await fetch(URL);
@@ -45,23 +46,46 @@ export default function Home() {
   };
 
   const postData = async () => {
-    const requestOptions = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: 'React PUT Request Example' })
+    setDate(new Date());
+    const ticket = {
+      "id": 1,
+      "equipmentId": Id,
+      "status": 'NEW',
+      "dateReported": {
+        year: date.getFullYear(),
+        month: date.getMonth() + 1, // add 1 because getMonth() returns 0-based index
+        day: date.getDate(),
+        hour: date.getHours(),
+        minute: date.getMinutes()
+      },
+      "priority": priority,
+      "description": null,
+      "assignedTo": null,
+      "dateResolved": null,
+      "resolutionDetails": null,
+      "notes": null
     };
-    const res = await fetch(URL, requestOptions);
+    console.log(JSON.stringify(ticket));
+    const requestOptions = {
+      method: 'POST',
+      headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify(ticket)
+    };
+    fetch(URL, requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(JSON.stringify(data)))
+      .catch(error => console.log(error));
   };
 
   const handleIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setId(event.target.value);
   };
-  const handleSevChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSev(event.target.value);
+  const handlePriorityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPriority(event.target.value);
   };
-  /* const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDate(event.target.value);
-  }; */
 
 
   return (
@@ -73,47 +97,51 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div>
-            <h1 className={styles.title}>
-              Welcome to URepair!
-            </h1>
-            <Box
-              component="form"
-              sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField
-                id="standard-basic"
-                label="Equipment-Id"
-                value={Id}
-                onChange={handleIdChange}
-              />
-              <TextField
-                id="outlined-select-currency"
-                select
-                label="Severity"
-                defaultValue="LOW"
-                onChange={handleSevChange}
-              >
-                {severities.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-              <TextField
-                id="standard-basic"
-                label="Date"
-                value={date}
-                //onChange={handleDateChange}
-              />
-            </Box>
-            {Id}
-            {sev}
-        </div>
+        <h1 className={styles.title}>
+          Welcome to URepair!
+        </h1>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="standard-basic"
+            label="Equipment-Id"
+            value={Id}
+            onChange={handleIdChange}
+          />
+          <TextField
+            id="outlined-select-priority"
+            select
+            label="Priority"
+            defaultValue="LOW"
+            onChange={handlePriorityChange}
+          >
+            {priorities.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+            ))}
+          </TextField>
+          {/* <TextField
+            id="standard-basic"
+            label="Date"
+            value={date}
+            disabled
+            //onChange={handleDateChange}
+          /> */}
+        </Box>
+        {/* data && <p>{data}</p> */}
+        <Button 
+          variant="contained" 
+          onClick={postData}
+        >
+          Submit
+        </Button>
       </main>
     </>
   );
