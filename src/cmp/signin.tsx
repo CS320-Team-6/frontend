3bd82c806b01df49d156b096f66de34bbb9b8980
MAXPIL0T <maximilian.kuechen@gmail.com>
2023-05-03 03:50:29 -0400

    fix: import buffer

--- src/cmp/signin.tsx
+++ src/cmp/signin.tsx
@@ -11,6 +11,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Buffer } from 'buffer';
 import Staff from './staff';
 
 function Copyright(props: any) {
