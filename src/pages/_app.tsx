dbbaa75d8a823802a7b4290eeaaeacd3759025ac
gwarren3210 <gavrielewarren@gmail.com>
2023-05-08 12:43:24 -0400

    mewo

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-
-export default function App({ Component, pageProps }: AppProps) {
-  // explanation for the eslint error here: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md
-  // should it be ignored? I think so - Gavriel
-  return <Component {...pageProps} />;
-}
